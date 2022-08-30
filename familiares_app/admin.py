4188e06f697fb2c60e30eb9ce965bd2799034021
Adrian Purves <adrianpurves@gmail.com>
2022-08-30 00:34:39 -0300

    BASE de DATos LISTA INFORMACION MOSTRADA EN EL INDEX

--- familiares_app/admin.py
+++ familiares_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from familiares_app.models import Familiar
 # Register your models here.
+admin.site.register(Familiar)
--- familiares_app/views.py
+++ familiares_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import request
+from familiares_app.models import Familiar
 # Create your views here.
-def home(req):
-    return render(req, "index.html", )
+def home(request):
+    queryset = Familiar.objects.all()
+    context= {"queryset":queryset}
+    return render(request, "index.html", context)
